e11144a7f0924e94536855af0c9976bad3bf0d34
Vladimir L <vladimir_l@gmx.net>
2017-02-21 22:24:08 +0100

    bash script to install ArchLinux on SD card

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# install-archlinuxarm
-Script to automate installation of ArchLinux ARM to SD card
+# Install ArchLinux ARM
+
+Script to automate installation of ArchLinux ARM to micro SD card.
+
+The script is based on [CuBox-i Installation Process](https://archlinuxarm.org/platforms/armv7/freescale/cubox-i)
+from ArchLinux ARM community web-site.
+
+## Important
+
+Script requires root permissions, or sudo rights.
+
+Be careful, since **you will loose all your data** on the device which name is passed to the script:
+ * Do a backup first!
+ * Supply a correct device name!
+ * You have 1 chance to interrupt the process.
+
+
+## Usage
+
+Run script with a device name as first parameter and follow the instructions.
+
+```bash
+$ sudo ./install_archlinux_cubox.sh sdb
+
+Going to install ArchLinux (CuBox-i) on device '/dev/sdb'
+
+All data on this device will be lost!
+Do you want to continue? (y/N):
+...
+```
---
+++ install_archlinux_cubox.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+device=$1
+image="ArchLinuxARM-imx6-cubox-latest.tar.gz"
+
+if [ -z "${device}" ]; then
+	echo "Please define device name"
+	echo "Usage:"
+	echo "    $0 <device>"
+	echo
+	echo "Example:"
+	echo "    $0 sdb"
+	exit 1
+fi
+
+echo
+echo "Going to install ArchLinux (CuBox-i) on device '/dev/${device}'"
+echo
+fdisk -l /dev/${device}
+
+echo
+echo "All data on this device will be lost!"
+echo -n "Do you want to continue? (y/N): "
+
+read confirm
+
+if [ ! "${confirm}" = "y" ]; then
+	echo "-- interrupted --"
+	exit 2
+fi
+
+echo
+
+if [ ! -f "${image}" ]; then
+	echo "Downloading latest CuBox image..."
+	wget http://os.archlinuxarm.org/os/${image}
+fi
+
+echo "Preparing device..."
+
+umount /dev/${device}1
+dd if=/dev/zero of=/dev/${device} bs=1M count=4
+
+echo "Type 'n', and press ENTER, accepting default values. Type 'w' to write changes and exit."
+fdisk /dev/${device}
+mkfs.ext4 /dev/${device}1
+mkdir -p root
+mount /dev/${device}1 root
+bsdtar -xpf ${image} -C root
+
+echo "Installing U-Boot bootloader..."
+dd if=root/boot/SPL of=/dev/${device} bs=1K seek=1
+dd if=root/boot/u-boot.img of=/dev/${device} bs=1K seek=42
+
+echo "Syncing..."
+sync
+umount root
+
+echo "Done!"
